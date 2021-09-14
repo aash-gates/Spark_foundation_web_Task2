e58b5c41d7048d5e29916758350c1531ad43672f
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-14 21:11:45 +0530

    c

---
+++ Basic_Banking_System-master/50-50/Anonymous Functions/src/main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val f:(Int,Int)->Int ={a,b -> a+b}
+    val result =f(8, 42)
+    println(result)
+}
---
+++ Basic_Banking_System-master/50-50/Arthematic Operation/src/main.kt
@@ -0,0 +1,14 @@
+/* Sololern
+ * Arithmetic Operation
+ */
+fun main(){
+    var num1 = 8
+    var num2 = 34
+    println(num1 + num2)  //Addition
+    println(num1 - num2)  //Subtraction
+    println(num1 * num2)  //Multiplication
+    println(num2 / num1)  //Division
+    println(num1 % num2) //Modules
+}
+
+//End of the Program
